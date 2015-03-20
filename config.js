a1fecc3204e7ae317c090ccee666261391e71f7b
witwave <linxuwit@gmail.com>
2015-03-20 16:48:28 +0800

    Revert "add log"
    This reverts commit fc6d70dc4ae260b2c69787f9b62809bfad9346f0.

--- config.js
+++ config.js
@@ -5,33 +5,7 @@
             version: "1.0.0"
         },
         mongo: "mongodb://192.168.70.138:27017/wbs",
-        tokenSecret: 'xxxxx',
-        loger: {
-            "appenders": [{
-                type: "console"
-            }, {
-                type: "file",
-                filename: "logs/access.log",
-                category: 'file'
-            }, {
-                "type": "smtp",
-                "recipients": "logfilerecipient@logging.com",
-                "sendInterval": 5,
-                "transport": "SMTP",
-                "SMTP": {
-                    "host": "smtp.gmail.com",
-                    "secureConnection": true,
-                    "port": 465,
-                    "auth": {
-                        "user": "someone@gmail",
-                        "pass": "********************"
-                    },
-                    "debug": true
-                },
-                "category": "mailer"
-            }],
-            replaceConsole: true
-        }
+        tokenSecret: 'wbs'
     },
     prd: {
         app: {
@@ -39,33 +13,7 @@
             version: "1.0.0"
         },
         mongo: "mongodb://127.0.0.1:27017/api",
-        tokenSecret: 'xxxxxx',
-        loger: {
-            "appenders": [{
-                type: "console"
-            }, {
-                type: "file",
-                filename: "logs/access.log",
-                category: 'file'
-            }, {
-                "type": "smtp",
-                "recipients": "logfilerecipient@logging.com",
-                "sendInterval": 5,
-                "transport": "SMTP",
-                "SMTP": {
-                    "host": "smtp.gmail.com",
-                    "secureConnection": true,
-                    "port": 465,
-                    "auth": {
-                        "user": "someone@gmail",
-                        "pass": "********************"
-                    },
-                    "debug": true
-                },
-                "category": "mailer"
-            }],
-            replaceConsole: true
-        }
+        tokenSecret: 'wbs'
     }
 };
 
